7e293f8d33db08706a30e74d63f9f083c0df4a1a
Brian Hoy <brianhoy4@gmail.com>
2016-10-01 09:47:15 -0400

    Added Object3D

--- ProceduralTerrain/Object3D.h
+++ ProceduralTerrain/Object3D.h
@@ -7,25 +7,73 @@ class Object3D {
 public:
 	glm::vec3 position;
 	glm::quat quaternion;
-	glm::vec3 rotation;
+	glm::vec3 eulerRotation; /* Write-only within this class */
 	glm::vec3 scale;
-	glm::mat4 matrix;
+
+	glm::mat4 translationMatrix;
+	glm::mat4 rotationMatrix;
+	glm::mat4 scaleMatrix;
+
+	glm::mat4 modelMatrix;
 
 	Object3D() {
-		quaternion = glm::quat(0.0f, 0.0f, 0.0f, 0.0f);
-	}
+		quaternion = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+		eulerRotation = glm::vec3(0.0f, 0.0f, 0.0f);
+		position = glm::vec3(0.0f, 0.0f, 0.0f);
+		scale = glm::vec3(0.0f, 0.0f, 0.0f);
 
-	void rotate(double x, double y, double z) {
-		rotation.x = x;
-		rotation.y = y;
-		rotation.z = z;
-		matrix = glm::rotate(matrix, 90.0f, rotation);
-	}
+		translationMatrix = glm::mat4();
+		rotationMatrix = glm::mat4();
+		scaleMatrix = glm::mat4();
 
+		updateMatrix();
+	}
+	void rotate(float angle, glm::vec3& axis) {
+		quaternion += glm::angleAxis(angle, axis);
+		eulerRotation = glm::eulerAngles(quaternion);
+
+		updateMatrix();
+	}
+	void rotate(glm::vec3& eulerAngles) {
+		quaternion += glm::angleAxis(eulerAngles.x, glm::vec3(1.0f, 0.0f, 0.0f));
+		quaternion += glm::angleAxis(eulerAngles.y, glm::vec3(0.0f, 1.0f, 0.0f));
+		quaternion += glm::angleAxis(eulerAngles.z, glm::vec3(0.0f, 0.0f, 1.0f));
+		rotationMatrix += glm::mat4(quaternion);
+		eulerRotation = glm::eulerAngles(quaternion);
+
+		updateMatrix();
+	}
+	void setAngles(glm::vec3& eulerAngles) {
+		quaternion = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+		rotate(eulerAngles);
+
+		updateMatrix();
+	}
+	void setAngles(glm::quat quaternion_) {
+		quaternion = quaternion_;
+		eulerRotation = glm::eulerAngles(quaternion);
+
+		updateMatrix();
+	}
+	void setScale(glm::vec3 scale_) {
+		scale = scale_;
+		scaleMatrix = glm::scale(scaleMatrix, scale);
+
+		updateMatrix();
+	}
+	void translate(glm::vec3& translation) {
+		position += translation;
+		translationMatrix = glm::translate(translationMatrix, translation);
+
+		updateMatrix();
+	}
+	void setPosition(glm::vec3 position_) {
+		position = position_;
+		translationMatrix = glm::translate(glm::mat4(), position);
+
+		updateMatrix();
+	}
 	void updateMatrix() {
-		matrix = glm::translate(matrix, position);
-		matrix = glm::rotate(matrix, rotation.x, glm::vec3(0.0f, 1.0f, 0.0f));
-		matrix = glm::rotate(matrix, rotation.y, glm::vec3(1.0f, 0))
-		
+		modelMatrix = translationMatrix * rotationMatrix * scaleMatrix;
 	}
 };
